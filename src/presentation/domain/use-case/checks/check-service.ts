facdbb1fce4b37f5589e178cd4316a5b101366c0
davidperezboza <david.perez@babelgroup.com>
2025-03-14 15:06:27 +0100

    Video 115

--- src/presentation/domain/use-case/checks/check-service.ts
+++ src/presentation/domain/use-case/checks/check-service.ts
@@ -2,19 +2,29 @@ interface CheckServiceUseCase{
     execute(url: string):Promise<boolean>
 };
 
+type SuccesCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUseCase {
+    constructor(
+        private readonly succesCallback: SuccesCallback,
+        private readonly errorCallback: ErrorCallback
+    )
+    {};
+
     async execute(url: string): Promise<boolean> {
         try {
             const req = await fetch(url);
             if(!req.ok){
                 throw new Error(`Error on check service ${url}`);
             }
-            console.log(`${url} is ok`);
+            this.succesCallback();
             return true;
         } catch (error) {
             console.log(`${error}`);
+            this.errorCallback(`${error}`)
             return false;
         }
-    }
+    };
     
 };
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,10 +4,14 @@ import { CheckService } from "./domain/use-case/checks/check-service";
 export class Server {
     public static start(){
         console.log('Server started..');
+        const url = 'http://google.com';
         CronService.createJob(
             '*/5 * * * * *',
             () => {
-              new CheckService().execute('http://google.com'); 
+              new CheckService(
+                () => console.log(`${url} is ok`),
+                (error) => console.log(error)
+              ).execute(url); 
               //new CheckService().execute('http://localhost:3000/'); 
             }
         );
