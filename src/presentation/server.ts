694b49f1b5a32edc8f800fa28873804d419d01e9
davidperezboza <david.perez@babelgroup.com>
2025-03-14 12:12:59 +0100

    Video 111

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,17 @@
+import {CronJob} from 'cron';
+
 export class Server {
     public static start(){
         console.log('Server started..');
+
+        const job = new CronJob(
+            '*/3 * * * * *', 
+            () => {
+                const date = new Date();
+                console.log('3 second', date.toString());
+            }, 
+        );
+
+        job.start();
     }
 };
