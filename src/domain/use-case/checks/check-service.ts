3279ac61b7c950683ad0571f096b6ece18fd43e2
davidperezboza <david.perez@babelgroup.com>
2025-03-18 21:30:53 +0100

    Video 138

--- src/domain/use-case/checks/check-service.ts
+++ src/domain/use-case/checks/check-service.ts
@@ -17,18 +17,27 @@ export class CheckService implements CheckServiceUseCase {
     {};
 
     async execute(url: string): Promise<boolean> {
+        const origin = 'check-service.ts';
         try {
             const req = await fetch(url);
             if(!req.ok){
                 throw new Error(`Error on check service ${url}`);
             }
-            const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
+            const log = new LogEntity({
+                message: `Service ${url} working`, 
+                level: LogSeverityLevel.low, 
+                origin,
+            });
             this.logRepository.saveLog(log)
             this.succesCallback && this.succesCallback();
             return true;
         } catch (error) {
             const ErrorMessage = `${url} is not ok. ${error}`;
-            const log = new LogEntity(ErrorMessage, LogSeverityLevel.high);
+            const log = new LogEntity({
+                message: ErrorMessage, 
+                level: LogSeverityLevel.high, 
+                origin,
+            });
             this.logRepository.saveLog(log);
             this.errorCallback && this.errorCallback(ErrorMessage)
             return false;
