d795f49178e50b02ef8ce6006582d86bf02ce4ff
davidperezboza <david.perez@babelgroup.com>
2025-03-20 14:13:34 +0100

    Video 141

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,8 @@ import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-case/checks/check-service";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { FyleSystemDataSource } from "../infrastructure/datasources/filye-system.datasource";
+import { envs } from "../config/plugins/envs.plugin";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FyleSystemDataSource(),
@@ -11,6 +13,18 @@ export class Server {
     public static start(){
         console.log('Server started..');
         const url = 'http://google.com/';
+
+        //Mandar email
+        //const emailService = new EmailService()
+        //emailService.sendEmail({
+        //  to: envs.MAILER_SEND_EMAIL,
+        //  subject: 'Logs de sistema',
+        //  htmlBody: `
+        //    <h3>Logs de sistema - NOC</h3>
+        //    <p>Culpa aliqua cillum aliquip culpa amet laborum officia cillum consequat ea non anim non. Elit non id quis dolor laboris. Consectetur culpa amet in sint minim deserunt mollit duis amet mollit esse mollit do.</p>
+        //    <p>Ver logs adjuntos</p>
+        //  `,
+        //});
         //CronService.createJob(
         //    '*/5 * * * * *',
         //    () => {
